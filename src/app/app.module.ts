69f0c20f7febf38a9160b0d825cb87ef474f32f0
Aswanth P <Aswanth@NTP-LAP-734.chn.nestgroup.net>
2022-11-24 16:04:36 +0530

    employee

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AdminLoginComponent } from './admin-login/admin-login.component';
 import { SearchEmployeeComponent } from './search-employee/search-employee.component';
+import { AddEmployeeComponent } from './add-employee/add-employee.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AdminLoginComponent,
-    SearchEmployeeComponent
+    SearchEmployeeComponent,
+    AddEmployeeComponent
   ],
   imports: [
     BrowserModule,
